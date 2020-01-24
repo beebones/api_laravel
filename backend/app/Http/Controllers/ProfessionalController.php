05ed86c35eb9afdca52406d26122e3677a535cb8
beebones <gelbundschwarz@gmail.com>
2020-01-24 12:10:45 -0300

    adicionando middleware cors e id da tecnologia

--- backend/app/Http/Controllers/ProfessionalController.php
+++ backend/app/Http/Controllers/ProfessionalController.php
@@ -4,20 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Professional;
+use App\Technology;
 
 class ProfessionalController extends Controller
 {
     public function listProfessionals(Request $request) {
-        $listProfessionals = Professional::all();
-        return response()->json($listProfessionals);
+        $listProfessionals = Professional::find(1);
+        return response()->json($listProfessionals->technologies);
     }
 
     public function createProfessional(Request $request) {
+        $technologyID = $request->technology;
         $newProfessional = new Professional();
         $newProfessional->name = $request->name;
-        $newProfessional->github = $request->github;
+        $newProfessional->github = $request->github;        
         $result = $newProfessional->save();
-
+        $technology = Technology::find($technologyID);
+        if($technology) {
+            $technology->professionals()->attach($newProfessional->id);
+        } else {
+            return response()->json(["error"=>"O id da tecnologia não existe"]);
+        }
+    
         return response()->json($newProfessional);
     }
 }
